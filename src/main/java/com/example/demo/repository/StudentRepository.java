35971ad0f106461e405b35b76e59814fa758aff5
bala-amypo <bala@amypo.in>
2025-12-24 05:25:00 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -6,5 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long>{
-
+// sabarivel lusu punda
+// vaa machanae machanae thee vechanae vechanae
+vaa di pulla vaa di vaa di pulla vaa di unna mattum nenachiruken en nenjukulla vaa nila nila nee enthan venila vaa nila nila nee enthan v
 }
